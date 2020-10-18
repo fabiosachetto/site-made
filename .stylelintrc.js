8a6dd52bd06ab652b21facf8962c448005f0563d
Fabio Galdo <fabiogaldo@gmail.com>
2020-10-18 01:44:24 -0300

    Animations

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,9 +1,9 @@
 module.exports = {
-  extends: "stylelint-config-standard",
-  plugins: ["stylelint-scss"],
+  extends: 'stylelint-config-standard',
+  plugins: ['stylelint-scss'],
   rules: {
-    "at-rule-no-unknown": null,
-    "scss/at-rule-no-unknown": true,
-    "no-descending-specificity": null,
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': true,
+    'no-descending-specificity': null,
   },
 };
--- src/javascripts/scripts.js
+++ src/javascripts/scripts.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 // Load jQuery from NPM
 import $ from 'jquery';
 
@@ -7,13 +8,61 @@ window.$ = $;
 const hueFunction = (hueVal) => {
   const setAs = `${hueVal}deg`;
   document
-    .getElementById('main-image')
+    .getElementById('main-video')
     .setAttribute('style', `filter:hue-rotate(${setAs}) saturate(1.5)`);
 };
 
+const rangeSlider = document.getElementById('hue-rotate');
+rangeSlider.addEventListener('change', hueFunction(this));
+
 window.hueFunction = hueFunction;
 
-$(function () {
-  const rangeSlider = document.getElementById('hue-rotate');
-  rangeSlider.addEventListener('change', hueFunction(this.value));
+const video = document.getElementById('main-video');
+// const items = $('#main nav ul li');
+
+video.onloadeddata = () => {
+  // alert('oi');
+  // video.play();
+  rangeSlider.setAttribute('value', 128);
+};
+video.addEventListener('ended', () => {
+  console.log('end');
+  const items = $('#main nav ul li');
+  items.addClass('slide-in-blurred-left');
 });
+
+video.addEventListener('timeupdate', () => {
+  // console.log(video.currentTime);
+  if (Math.round(video.currentTime) === 2) {
+    console.log('OK');
+    $('.intro__img-holder').addClass('swing-in-top-fwd ');
+    video.removeEventListener('timeupdate', () => {});
+  }
+});
+
+// const items = $('#main nav ul li');
+// $.each(items, function () {
+//   setTimeout(() => {
+//     $(this).addClass('slide-in-blurred-left');
+//   }, 1000);
+// });
+
+// $('#main nav ul li').each((el) => {
+//   $(el).delay(500).addClass('slide-in-blurred-left');
+// });
+
+// $('#main nav ul li').each((el) => {
+//   console.log(el);
+//   $(el).delay(1000).addClass('slide-in-blurred-left');
+// });
+
+// $(() => {
+
+// });
+
+// items.each((el) => {
+//   setTimeout(() => {
+//     console.log(el);
+//     $(el).addClass('slide-in-blurred-left');
+//   }, 1000);
+// });
