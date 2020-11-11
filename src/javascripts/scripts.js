3fface8e1ee5663c6287067f14ab50387b2b9ac8
Fabio Galdo <fabiogaldo@gmail.com>
2020-11-11 20:36:50 -0300

    scripts

--- src/javascripts/scripts.js
+++ src/javascripts/scripts.js
@@ -1,10 +1,14 @@
 /* eslint-disable no-console */
 // Load jQuery from NPM
 import $ from 'jquery';
+// import simpleParallax from 'simple-parallax-js';
 import Home from './index';
 
 window.jQuery = $;
 window.$ = $;
 window.Home = Home;
+
+// const image = document.getElementById('info');
+// simpleParallax(image);
 
 // Home();
